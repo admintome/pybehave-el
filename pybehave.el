e42212ad4f1971371949e4af3a24b1d785880839
Bill Ward <bill@admintome.com>
2018-02-22 16:54:00 -0600

    initial commit

---
+++ pybehave.el
@@ -0,0 +1,24 @@
+;;; pybehave.el --- Easy Python Behave test running in Emacs
+
+;; Copyright (C) 2018 Bill Ward
+
+;; Licensed under the same terms as Emacs.
+
+;; Version: 0.0.1
+;; Keywords: behave python testing
+;; URL: https://github.com/admintome/pybehave-el
+;; Package-Requires: ((s "1.9.0"))
+;; Created: 22 Feb 2018
+
+;; This file is NOT part of GNU Emacs.
+
+;; Licensed under the same terms as Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'pybehave)
+
+;;; pybehave.el ends here
